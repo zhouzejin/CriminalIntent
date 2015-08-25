37e965dac4e1ee4f75a0816e2f1a415cf64b91fa
Zhou Zejin <zjzhou1@chinaums.com>
2015-08-25 12:23:51 +0800

    完成第19章 取景器

--- src/com/sunny/criminalintent/CrimeCameraActivity.java
+++ src/com/sunny/criminalintent/CrimeCameraActivity.java
@@ -1,8 +1,21 @@
 package com.sunny.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CrimeCameraActivity extends SingleCrimeActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// Hide the window title.
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// Hide the status bar and other OS-level chrome
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		super.onCreate(savedInstanceState);
+	}
 
 	@Override
 	protected Fragment createFragment() {
